37d8d2c6d23853db6e415923e752f3b72a53720f
msvt1982 <msvt1982@gmail.com>
2025-09-21 10:05:36 +0300

    homework 5 - mvvm. Changes Post data class

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,8 +5,8 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likes: Int = 1_099_999,
-    var likedByMe: Boolean = false,
-    var share: Int = 9_999,
-    var postViews: Int = 5
+    val likes: Int = 1_099_999,
+    val likedByMe: Boolean = false,
+    val share: Int = 9_999,
+    val postViews: Int = 5
 )
